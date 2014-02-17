9713d32c606d40aa3985e0a54aaabee767b0048d
wstampley <wstampley@gmail.com>
2014-02-16 19:05:51 -0600

    Main Menu-ish Looking Thing

---
+++ Assets/Scripts/MainMenuBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update () 
+	{
+		if (Input.GetMouseButtonUp (0))
+		{
+			Ray touchTestRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hitInfo;
+			if (Physics.Raycast(touchTestRay, out hitInfo))
+			{
+				GameObject touchedObject = hitInfo.collider.gameObject;
+
+			}
+		}
+	}
+}
